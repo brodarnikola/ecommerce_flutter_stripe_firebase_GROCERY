08f8d4c146d02ffbf787041d9e3bd3bd6e62906f
Nikola Brodar <nikolabrodar@Nikolas-MBP.home>
2024-07-12 11:06:36 +0200

    Fetch payments only when user enter this screen

--- lib/screens/transactions/transactions.dart
+++ lib/screens/transactions/transactions.dart
@@ -45,8 +45,8 @@ class _TransactionsScreenState extends State<TransactionsScreen> {
     final themeState = Utils(context).getTheme;
     // Size size = Utils(context).getScreenSize;
 
-    final paymentsProvider = Provider.of<TransactionsProvider>(context);
-    final paymentsList = paymentsProvider.getTransactions;
+    final transactionsProvider = Provider.of<TransactionsProvider>(context);
+    final transactionsList = transactionsProvider.getTransactions;
 
     return Scaffold(
         appBar: AppBar(
@@ -54,7 +54,7 @@ class _TransactionsScreenState extends State<TransactionsScreen> {
           elevation: 0,
           centerTitle: false,
           title: TextWidget(
-            text: 'Your transactions (${paymentsList.length})',
+            text: 'Your transactions (${transactionsList.length})',
             color: color,
             textSize: 24.0,
             isTitle: true,
@@ -63,96 +63,107 @@ class _TransactionsScreenState extends State<TransactionsScreen> {
               Theme.of(context).scaffoldBackgroundColor.withOpacity(0.9),
         ),
         body: LoadingManager(
-            isLoading: paymentsProvider.isLoading,
+            isLoading: transactionsProvider.isLoading,
             child: Stack(children: [
-              paymentsList.isEmpty
-                  ? const EmptyScreen(
-                      title: 'You dont have any transactions!',
-                      subtitle: '',
-                      buttonText: 'Back',
-                      imagePath: 'assets/images/cart.png',
-                      route: '/UserScreen',
-                      navigateBack: true,
-                    )
-                  : ListView.separated(
-                      padding: const EdgeInsets.all(5),
-                      itemCount: paymentsList.length,
-                      itemBuilder: (ctx, index) {
-                        return Container(
-                          decoration: BoxDecoration(
-                            color: Colors.blue[50],
-                            borderRadius: BorderRadius.circular(20),
-                            border: Border.all(
-                                color: Colors.grey[300] ?? Colors.grey),
-                          ),
-                          padding: const EdgeInsets.symmetric(
-                              horizontal: 20, vertical: 25),
-                          //  padding: const EdgeInsets.all(20.0),
-                          child: ChangeNotifierProvider.value(
-                            value: paymentsList[index],
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                TextWidget(
-                                    text:
-                                        '${paymentsList[index].parkingTvrtkaNaziv} - ${paymentsList[index].garazaNaziv}',
-                                    color: color,
-                                    textSize: 18),
-                                const SizedBox(
-                                  height: 2,
+              // paymentsProvider.isLoading
+              //     ?
+              //     //const Center(
+
+              //     //     // child: CircularProgressIndicator(),
+              //     //   )
+              //     Container(
+              //         color: Colors.black.withOpacity(0.7),
+              //       )
+              //     : 
+                  transactionsList.isEmpty
+                      ? const EmptyScreen(
+                          title: 'You dont have any transactions!',
+                          subtitle: '',
+                          buttonText: 'Back',
+                          imagePath: 'assets/images/cart.png',
+                          route: '/UserScreen',
+                          navigateBack: true,
+                        )
+                      : ListView.separated(
+                          padding: const EdgeInsets.all(5),
+                          itemCount: transactionsList.length,
+                          itemBuilder: (ctx, index) {
+                            return Container(
+                              decoration: BoxDecoration(
+                                color: Colors.blue[50],
+                                borderRadius: BorderRadius.circular(20),
+                                border: Border.all(
+                                    color: Colors.grey[300] ?? Colors.grey),
+                              ),
+                              padding: const EdgeInsets.symmetric(
+                                  horizontal: 20, vertical: 25),
+                              //  padding: const EdgeInsets.all(20.0),
+                              child: ChangeNotifierProvider.value(
+                                value: transactionsList[index],
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    TextWidget(
+                                        text:
+                                            '${transactionsList[index].parkingTvrtkaNaziv} - ${transactionsList[index].garazaNaziv}',
+                                        color: color,
+                                        textSize: 18),
+                                    const SizedBox(
+                                      height: 2,
+                                    ),
+                                    TextWidget(
+                                        text:
+                                            '${transactionsList[index].registracija}',
+                                        color: color,
+                                        textSize: 18),
+                                    const SizedBox(
+                                      height: 2,
+                                    ),
+                                    TextWidget(
+                                        text:
+                                            '${transactionsList[index].parkingTvrtkaNaziv}',
+                                        color: color,
+                                        textSize: 18),
+                                    const SizedBox(
+                                      height: 2,
+                                    ),
+                                    TextWidget(
+                                        text:
+                                            'From ${GlobalMethods.getDateFromDateTimeString(transactionsList[index].start ?? "")} - ${GlobalMethods.getTimeFromDateTimeString(transactionsList[index].start ?? "")}',
+                                        color: color,
+                                        textSize: 18),
+                                    const SizedBox(
+                                      height: 2,
+                                    ),
+                                    TextWidget(
+                                        text:
+                                            'Until ${GlobalMethods.getDateFromDateTimeString(transactionsList[index].end ?? "")} - ${GlobalMethods.getTimeFromDateTimeString(transactionsList[index].end ?? "")}',
+                                        color: color,
+                                        textSize: 18),
+                                    const SizedBox(
+                                      height: 2,
+                                    ),
+                                    TextWidget(
+                                        text:
+                                            'Amount: ${(transactionsList[index].amount ?? 1) / 100} EUR',
+                                        color: color,
+                                        textSize: 18),
+                                  ],
                                 ),
-                                TextWidget(
-                                    text: '${paymentsList[index].registracija}',
-                                    color: color,
-                                    textSize: 18),
-                                const SizedBox(
-                                  height: 2,
-                                ),
-                                TextWidget(
-                                    text:
-                                        '${paymentsList[index].parkingTvrtkaNaziv}',
-                                    color: color,
-                                    textSize: 18),
-                                const SizedBox(
-                                  height: 2,
-                                ),
-                                TextWidget(
-                                    text:
-                                        'From ${GlobalMethods.getDateFromDateTimeString(paymentsList[index].start ?? "")} - ${GlobalMethods.getTimeFromDateTimeString(paymentsList[index].start ?? "")}',
-                                    color: color,
-                                    textSize: 18),
-                                const SizedBox(
-                                  height: 2,
-                                ),
-                                TextWidget(
-                                    text:
-                                        'Until ${GlobalMethods.getDateFromDateTimeString(paymentsList[index].end ?? "")} - ${GlobalMethods.getTimeFromDateTimeString(paymentsList[index].end ?? "")}',
-                                    color: color,
-                                    textSize: 18),
-                                const SizedBox(
-                                  height: 2,
-                                ),
-                                TextWidget(
-                                    text:
-                                        'Amount: ${(paymentsList[index].amount ?? 1) / 100} EUR',
-                                    color: color,
-                                    textSize: 18),
-                              ],
-                            ),
-                            // child: VehicleWidget( index: index), // Pass the index as a named argument
-                          ),
-                        );
-                      },
-                      separatorBuilder: (BuildContext context, int index) {
-                        return const SizedBox(
-                          height: 10,
-                        );
-                        // return Divider(
-                        //   color: color,
-                        //   thickness: 0,
-                        // );
-                      },
-                    )
+                                // child: VehicleWidget( index: index), // Pass the index as a named argument
+                              ),
+                            );
+                          },
+                          separatorBuilder: (BuildContext context, int index) {
+                            return const SizedBox(
+                              height: 10,
+                            );
+                            // return Divider(
+                            //   color: color,
+                            //   thickness: 0,
+                            // );
+                          },
+                        )
             ])));
   }
 }
